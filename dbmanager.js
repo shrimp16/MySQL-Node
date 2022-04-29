02803719dd3805e825c032780cd5b52712737ad3
Shrimp <lcamara0916@gmail.com>
2022-04-29 18:22:02 +0000

    Refactor and added examples of queries to study

--- dbmanager.js
+++ dbmanager.js
@@ -1,5 +1,22 @@
 const mysql = require('mysql');
 
+const CREATE_PARENT_TABLE = 'CREATE TABLE parent(id int AUTO_INCREMENT, name VARCHAR(255) UNIQUE, password VARCHAR(255), PRIMARY KEY(id))'
+
+const CREATE_CHILD_TABLE = 'CREATE TABLE child(childID int AUTO_INCREMENT, game VARCHAR(255), parentID int, PRIMARY KEY(childID), FOREIGN KEY(parentID) REFERENCES parent(id))'
+
+const ADD_ROW_PARENT = 'INSERT INTO parent SET ?'
+const CONTENT_PARENT = {
+    name: 'name',
+    password: 'password'
+}
+
+const ADD_ROW_CHILD = 'INSERT INTO child SET ?'
+const CONTENT_CHILD = {
+    game: 'game',
+    parentID: 1
+}
+
+
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -12,17 +29,7 @@ db.connect((err) => {
     console.log('Connected to MySQL');
 })
 
-/*let sql = 'CREATE TABLE persons(id int AUTO_INCREMENT, name VARCHAR(255), age int, PRIMARY KEY(id))'
-
-db.query(sql, (err, result) => {
-    if(err) throw err;
-    console.log(result);
-})*/
-
-let person = {name: "Antonio", age: 50}
-let sql = 'INSERT INTO persons SET ?'
-
-db.query(sql, person, (err, result) => {
+db.query(ADD_ROW_CHILD, CONTENT_CHILD, (err, result) => {
     if(err) throw err;
     console.log(result);
 })

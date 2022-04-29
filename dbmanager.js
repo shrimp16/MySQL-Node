a9beaaeb70d71c080ec48129967d46d62edf1037
Shrimp <lcamara0916@gmail.com>
2022-04-29 12:09:29 +0000

    The script can now create tables in the database

--- dbmanager.js
+++ dbmanager.js
@@ -3,10 +3,18 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: ''
+    password: '',
+    database: 'tests'
 })
 
 db.connect((err) => {
     if(err) console.log(err.message);
     console.log('Connected to MySQL');
+})
+
+let sql = 'CREATE TABLE persons(id int AUTO_INCREMENT, name VARCHAR(255), age int, PRIMARY KEY(id))'
+
+db.query(sql, (err, result) => {
+    if(err) throw err;
+    console.log(result);
 })
